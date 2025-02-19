217e6f9decc7e3348140d59ddc56ba99b70ff7e6
Alice Ribeiro <aliceribeiro.work@gmail.com>
2025-02-18 23:32:26 -0300

    chore: add comments to improve code clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,26 @@
-import './globals.css'
+import "./globals.css";
 
 import type { Metadata } from "next";
 // Importa as fontes Montserrat e Oxanium do Google Fonts
-import {Montserrat , Oxanium} from 'next/font/google'
+import { Montserrat, Oxanium } from "next/font/google";
 
 export const metadata: Metadata = {
-  title: "devstage"
+  title: "devstage",
 };
 
 // Configura a fonte Oxanium com pesos 500 e 600 e define uma variável CSS
 const oxanium = Oxanium({
-  weight: ['500', '600'],
-  subsets: ['latin'],
-  variable: '--font-oxanium',
-})
+  weight: ["500", "600"],
+  subsets: ["latin"],
+  variable: "--font-oxanium",
+});
 
 // Configura a fonte Montserrat com pesos 400 e 600 e define uma variável CSS
 const montserrat = Montserrat({
-  weight: ['400', '600'],
-  subsets: ['latin'],
-  variable: '--font-montserrat',
-})
-
-
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export default function RootLayout({
   children,
@@ -32,7 +30,7 @@ export default function RootLayout({
   return (
     // Aplica as variáveis de fonte Oxanium e Montserrat à tag <html>
     <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
-      <body>{children}</body>
+      <body className="bg-gray-900 text-gray-100 antialiased ">{children}</body>
     </html>
   );
 }
